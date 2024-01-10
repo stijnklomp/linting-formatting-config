a181825dd35a95c38f0d10d26fa1703dcdf26309
stijnklomp <stijnklomp1@hotmail.com>
2024-01-10 00:10:57 +0000

    Update regex pattern for styling names

--- stylelint/stylelintRulesMui.js
+++ stylelint/stylelintRulesMui.js
@@ -1,20 +1,20 @@
 module.exports = {
 	rules: {
 		"selector-class-pattern": [
-			"Mui.*|(?:^[a-z0-9][a-zA-Z0-9]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9]*)*)$",
+			"Mui.*|^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
 				message: (selector) => `Expected class selector "${selector}" to be of BEM convention`
 			}
 		],
 		"selector-id-pattern": [
-			"Mui.*|(?:^[a-z0-9][a-zA-Z0-9]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9]*)*)$",
+			"Mui.*|^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
 				message: (selector) => `Expected id selector "${selector}" to be of BEM convention`
 			}
 		],
 		"color-no-invalid-hex": true,
 		"custom-property-pattern": [
-			"Mui.*|(?:^[a-z0-9][a-zA-Z0-9]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9]*)*)$",
+			"Mui.*|^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
 				message: (selector) => `Expected design token "${selector}" to be of BEM convention`
 			}
@@ -23,7 +23,7 @@ module.exports = {
 			"ignoreProperties": ["composes"]
 		}],
 		"scss/dollar-variable-pattern": [
-			"Mui.*|(?:^[a-z0-9][a-zA-Z0-9]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9]*)*)$",
+			"Mui.*|^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
 				message: (selector) => `Expected dollar variable pattern "${selector}" to be of BEM convention`
 			}
