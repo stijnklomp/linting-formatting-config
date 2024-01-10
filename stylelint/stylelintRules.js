57f457f6a69e1b9f159c524301ef4fa6f01fb288
stijnklomp <stijnklomp1@hotmail.com>
2024-01-10 19:38:22 +0000

    Update styelint regex description

--- stylelint/stylelintRules.js
+++ stylelint/stylelintRules.js
@@ -3,20 +3,20 @@ module.exports = {
 		"selector-class-pattern": [
 			"^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
-				message: (selector) => `Expected class selector "${selector}" to be of BEM convention`
+				message: (selector) => `Expected class selector "${selector}" to be of BEM convention with camel case`
 			}
 		],
 		"selector-id-pattern": [
 			"^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
-				message: (selector) => `Expected id selector "${selector}" to be of BEM convention`
+				message: (selector) => `Expected id selector "${selector}" to be of BEM convention with camel case`
 			}
 		],
 		"color-no-invalid-hex": true,
 		"custom-property-pattern": [
 			"^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
-				message: (selector) => `Expected design token "${selector}" to be of BEM convention`
+				message: (selector) => `Expected design token "${selector}" to be of BEM convention with camel case`
 			}
 		],
 		"property-no-unknown": [true, {
@@ -25,7 +25,7 @@ module.exports = {
 		"scss/dollar-variable-pattern": [
 			"^[a-z0-9][a-zA-Z0-9-]*(?:(?:_{2}|-{2})[a-z0-9][a-zA-Z0-9-]*)*$",
 			{
-				message: (selector) => `Expected dollar variable pattern "${selector}" to be of BEM convention`
+				message: (selector) => `Expected dollar variable pattern "${selector}" to be of BEM convention with camel case`
 			}
 		]
 	}
